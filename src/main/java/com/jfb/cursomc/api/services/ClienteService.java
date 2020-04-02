d8b3b22efbb56181f6b5cda605c71ad99db3a3f6
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-04-01 21:56:06 -0300

    PUT, DELETE e GET para Cliente.

--- src/main/java/com/jfb/cursomc/api/services/ClienteService.java
+++ src/main/java/com/jfb/cursomc/api/services/ClienteService.java
@@ -1,24 +1,70 @@
 package com.jfb.cursomc.api.services;
 
+import java.util.List;
 import java.util.Optional;
 
-import com.jfb.cursomc.api.domain.Cliente;
-import com.jfb.cursomc.api.repositories.ClienteRepository;
-import com.jfb.cursomc.api.services.exceptions.ObjectNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+
+import com.jfb.cursomc.api.domain.Cliente;
+import com.jfb.cursomc.api.dto.ClienteDTO;
+import com.jfb.cursomc.api.repositories.ClienteRepository;
+import com.jfb.cursomc.api.services.exceptions.DataIntegrityException;
+import com.jfb.cursomc.api.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
 
-    @Autowired
-    private ClienteRepository repo;
+	@Autowired
+	private ClienteRepository repo;
 
-    public Cliente find(Integer id) {
-        Optional<Cliente> obj = repo.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException(
-                "Objecto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objecto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
 
-    }
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId()); // Aqui estou verificando se o objeto existe.
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	// Metódo para instanciar uma Cliente a parti de um DTO.
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+
+	}
 }
