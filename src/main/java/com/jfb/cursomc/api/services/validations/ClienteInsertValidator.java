5262a34b4051d7ca3159a2779cefcc8875f9121f
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-04-02 03:51:08 -0300

    Validação customizada: Email repetido na inserção de Cliente.

--- src/main/java/com/jfb/cursomc/api/services/validations/ClienteInsertValidator.java
+++ src/main/java/com/jfb/cursomc/api/services/validations/ClienteInsertValidator.java
@@ -5,12 +5,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.jfb.cursomc.api.domain.Cliente;
 import com.jfb.cursomc.api.domain.enums.TipoCliente;
 import com.jfb.cursomc.api.dto.ClienteNewDTO;
+import com.jfb.cursomc.api.repositories.ClienteRepository;
 import com.jfb.cursomc.api.resources.exceptions.FieldMessage;
 import com.jfb.cursomc.api.services.validations.utils.BR;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
 
     @Override
     public void initialize(ClienteInsert ann) {
@@ -20,12 +27,19 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
     public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
         List<FieldMessage> list = new ArrayList<>();
 
-        // Lista de erros
+        // Validando CPF
         if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CPF inválido!"));
         }
+        // Validando CPF
         if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
+        }
+
+        // Validando Email
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
