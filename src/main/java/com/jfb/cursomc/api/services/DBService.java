296d7d7acd0efb1107b9653f94f226e45fe431e6
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-05-01 06:30:50 -0300

    Liberando CORS para PUT e DELETE.

--- src/main/java/com/jfb/cursomc/api/services/DBService.java
+++ src/main/java/com/jfb/cursomc/api/services/DBService.java
@@ -203,11 +203,11 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "nelio.cursos@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
+		Cliente cli1 = new Cliente(null, "Maria Silva", "sojogosvaldo@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
-		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOA_FISICA, pe.encode("123"));
+		Cliente cli2 = new Cliente(null, "Ana Costa", "rockval77@hotmail.com", "31628382740", TipoCliente.PESSOA_FISICA, pe.encode("123"));
 		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
 		cli2.addPerfil(Perfil.ADMIN);
 		
